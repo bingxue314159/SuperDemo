313e5eaeba551b21386e1811ee9a1fe5430fbdb0
谈宇刚 <bingxue314159@163.com>
2016-07-15 14:40:13 +0800

    优化网络封装

--- SuperDemo/Utility/TYG_Utility/TYGNetworkUtility.m
+++ SuperDemo/Utility/TYG_Utility/TYGNetworkUtility.m
@@ -9,16 +9,16 @@
 #import "TYGNetworkUtility.h"
 #import <netdb.h>
 #import <arpa/inet.h>
-#import <SystemConfiguration/SystemConfiguration.h>
-
+#import <netinet/in.h>
 #import <ifaddrs.h>   //获取设备IP地址
+
+#import <SystemConfiguration/SystemConfiguration.h>
+#import <SystemConfiguration/CaptiveNetwork.h>
 
 @implementation TYGNetworkUtility
 
-/*
+/**
  * 功能：检测网络连接状态
- * 备注：#import "SystemConfiguration/SystemConfiguration.h"
- *      #include "netdb.h"
  */
 +(BOOL) connectedToNetwork
 {
@@ -110,30 +110,6 @@
     return addressString;
 }
 
-/** 获取设备IP地址 */
-+ (NSString *)getIPAddress{
-    
-    NSString *address = nil;
-    struct ifaddrs *interfaces = NULL;
-    struct ifaddrs *temp_addr = NULL;
-    int success = 0;
-    success = getifaddrs(&interfaces);
-    if (success == 0) {
-        temp_addr = interfaces;
-        while (temp_addr != NULL) {
-            if (temp_addr->ifa_addr->sa_family == AF_INET) {
-                if ([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
-                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
-                }
-                temp_addr = temp_addr->ifa_next;
-            }
-        }
-    }
-    freeifaddrs(interfaces);
-    
-    return address;
-}
-
 /** 检测一个网址是否可以正常访问 */
 + (BOOL) hostAvailable: (NSString *) theHost
 {
@@ -198,5 +174,118 @@
     return address;
 }
 
+
+#pragma mark WIFI相关
+/**
+ *  获取WiFi 信息
+ *  返回的字典中包含了WiFi的名称、路由器的Mac地址、还有一个Data(转换成字符串打印出来是wifi名称)
+ *  e.g. { BSSID = "a4:2b:8c:c:7f:bd"; SSID = bdmy06; SSIDDATA = ;}
+ *  @return NSDictionary
+ */
++ (NSDictionary *)fetchSSIDInfo {
+    NSArray *ifs = (__bridge_transfer NSArray *)CNCopySupportedInterfaces();
+    if (!ifs) {
+        return nil;
+    }
+    NSDictionary *info = nil;
+    for (NSString *ifnam in ifs) {
+        info = (__bridge_transfer NSDictionary *)CNCopyCurrentNetworkInfo((__bridge CFStringRef)ifnam);
+        if (info && [info count]) {
+            // 这里其实对应的有三个key:kCNNetworkInfoKeySSID、kCNNetworkInfoKeyBSSID、kCNNetworkInfoKeySSIDData，
+            //WiFiName = [info objectForKey:(__bridge NSString *)kCNNetworkInfoKeySSID];
+            //WiFiName = [info objectForKey:@"SSID"];
+            break;
+        }
+    }
+    return info;
+}
+
+/**
+ *  获取广播地址、子网掩码、端口等
+ *  { broadcast = "192.168.1.255"; interface = en0; localIp = "192.168.1.7"; etmask = "255.255.255.0"; }
+ *  @return NSMutableDictionary
+ */
++ (NSMutableDictionary *)getLocalInfoForCurrentWiFi {
+    
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    
+    // retrieve the current interfaces - returns 0 on success
+    success = getifaddrs(&interfaces);
+    if (success == 0) {
+        // Loop through linked list of interfaces
+        temp_addr = interfaces;
+        //*/
+        while(temp_addr != NULL) {
+            if(temp_addr->ifa_addr->sa_family == AF_INET) {
+                // Check if interface is en0 which is the wifi connection on the iPhone
+                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
+                    
+                    //----192.168.1.255 广播地址
+                    NSString *broadcast = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_dstaddr)->sin_addr)];
+                    if (broadcast) {
+                        [dict setObject:broadcast forKey:@"broadcast"];
+                    }
+                    NSLog(@"broadcast address--%@",broadcast);
+                    
+                    //--192.168.1.106 本机地址
+                    NSString *localIp = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+                    if (localIp) {
+                        [dict setObject:localIp forKey:@"localIp"];
+                    }
+                    NSLog(@"local device ip--%@",localIp);
+                    
+                    //--255.255.255.0 子网掩码地址
+                    NSString *netmask = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_netmask)->sin_addr)];
+                    if (netmask) {
+                        [dict setObject:netmask forKey:@"netmask"];
+                    }
+                    NSLog(@"netmask--%@",netmask);
+                    
+                    //--en0 端口地址
+                    NSString *interface = [NSString stringWithUTF8String:temp_addr->ifa_name];
+                    if (interface) {
+                        [dict setObject:interface forKey:@"interface"];
+                    }
+                    NSLog(@"interface--%@",interface);
+                    return dict;
+                }
+            }
+            
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    
+    // Free memory
+    freeifaddrs(interfaces);
+    return dict;
+}
+
+/** 获取设备IP地址(WIFI) */
++ (NSString *)getLocalIPAddressForCurrentWiFi{
+    
+    NSString *address = nil;
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    
+    success = getifaddrs(&interfaces);
+    if (success == 0) {
+        temp_addr = interfaces;
+        while (temp_addr != NULL) {
+            if (temp_addr->ifa_addr->sa_family == AF_INET) {
+                if ([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+                }
+                temp_addr = temp_addr->ifa_next;
+            }
+        }
+    }
+    freeifaddrs(interfaces);
+    
+    return address;
+}
 
 @end

c68c6716fe79b130ef3e3bb15f7ddbf877377f88
谈宇刚 <bingxue314159@163.com>
2016-10-08 10:18:41 +0800

    适配iOS 10
    1.适配iOS 10
    2.实现UIButton的上图下文、上文下图、左图右文、右图左文的布局需求

--- SuperDemo/Utility/TYG_Utility/TYGUIButton/TYGUIButton.h
+++ SuperDemo/Utility/TYG_Utility/TYGUIButton/TYGUIButton.h
@@ -5,12 +5,17 @@
 //  Created by 谈宇刚 on 15/8/28.
 //  Copyright (c) 2015年 tanyugang. All rights reserved.
 //
-//  自定义不规则按钮的点击事件的响应区域
+/**
+ *  自定义不规则按钮的点击事件的响应区域
+ *  实现UIButton的上图下文、上文下图、左图右文、右图左文的布局需求
+ *
+ */
+
 
 #import <UIKit/UIKit.h>
 
 /**
- *  按钮样式
+ *  按钮变形样式
  */
 typedef NS_ENUM(NSInteger, TYGUIButtonType){
     
@@ -22,8 +27,21 @@ typedef NS_ENUM(NSInteger, TYGUIButtonType){
     TYGUIButtonTypeTriangleRight = 5    /**< 三角形按钮(▶) */
 };
 
+
+/**
+ 按钮样式（图片与文字的位置）
+ */
+typedef NS_ENUM(NSUInteger, JXLayoutButtonStyle) {
+    JXLayoutButtonStyleLeftImageRightTitle,
+    JXLayoutButtonStyleLeftTitleRightImage,
+    JXLayoutButtonStyleUpImageDownTitle,
+    JXLayoutButtonStyleUpTitleDownImage
+};
+
 @interface TYGUIButton : UIButton
 
-@property(nonatomic, assign) TYGUIButtonType tygButtonType;
+@property (nonatomic, assign) TYGUIButtonType tygButtonType;/**< 按钮变形样式 */
+@property (nonatomic, assign) JXLayoutButtonStyle layoutStyle;/**< 按钮布局方式 */
+@property (nonatomic, assign) CGFloat midSpacing;/**< 图片和文字的间距，默认值8 */
 
 @end
--- SuperDemo/Utility/TYG_Utility/TYGUIButton/TYGUIButton.m
+++ SuperDemo/Utility/TYG_Utility/TYGUIButton/TYGUIButton.m
@@ -10,6 +10,13 @@
 
 @implementation TYGUIButton
 
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        self.midSpacing = 8;
+    }
+    return self;
+}
+
 /**
  * 系统发生触摸事件的时候会从window到父控件到子控件一个个检测触摸点是否在其中
  * 如果在其中，则返回YES，最后返回YES的子控件作为响应事件的控件。
@@ -97,6 +104,70 @@
     }
     
     return flag;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    [self.imageView sizeToFit];
+    [self.titleLabel sizeToFit];
+    
+    switch (self.layoutStyle) {
+        case JXLayoutButtonStyleLeftImageRightTitle:
+            [self layoutHorizontalWithLeftView:self.imageView rightView:self.titleLabel];
+            break;
+        case JXLayoutButtonStyleLeftTitleRightImage:
+            [self layoutHorizontalWithLeftView:self.titleLabel rightView:self.imageView];
+            break;
+        case JXLayoutButtonStyleUpImageDownTitle:
+            [self layoutVerticalWithUpView:self.imageView downView:self.titleLabel];
+            break;
+        case JXLayoutButtonStyleUpTitleDownImage:
+            [self layoutVerticalWithUpView:self.titleLabel downView:self.imageView];
+            break;
+        default:
+            break;
+    }
+}
+
+- (void)layoutHorizontalWithLeftView:(UIView *)leftView rightView:(UIView *)rightView {
+    CGRect leftViewFrame = leftView.frame;
+    CGRect rightViewFrame = rightView.frame;
+    
+    CGFloat totalWidth = CGRectGetWidth(leftViewFrame) + self.midSpacing + CGRectGetWidth(rightViewFrame);
+    
+    leftViewFrame.origin.x = (CGRectGetWidth(self.frame) - totalWidth) / 2.0;
+    leftViewFrame.origin.y = (CGRectGetHeight(self.frame) - CGRectGetHeight(leftViewFrame)) / 2.0;
+    leftView.frame = leftViewFrame;
+    
+    rightViewFrame.origin.x = CGRectGetMaxX(leftViewFrame) + self.midSpacing;
+    rightViewFrame.origin.y = (CGRectGetHeight(self.frame) - CGRectGetHeight(rightViewFrame)) / 2.0;
+    rightView.frame = rightViewFrame;
+}
+
+- (void)layoutVerticalWithUpView:(UIView *)upView downView:(UIView *)downView {
+    CGRect upViewFrame = upView.frame;
+    CGRect downViewFrame = downView.frame;
+    
+    CGFloat totalHeight = CGRectGetHeight(upViewFrame) + self.midSpacing + CGRectGetHeight(downViewFrame);
+    
+    upViewFrame.origin.y = (CGRectGetHeight(self.frame) - totalHeight) / 2.0;
+    upViewFrame.origin.x = (CGRectGetWidth(self.frame) - CGRectGetWidth(upViewFrame)) / 2.0;
+    upView.frame = upViewFrame;
+    
+    downViewFrame.origin.y = CGRectGetMaxY(upViewFrame) + self.midSpacing;
+    downViewFrame.origin.x = (CGRectGetWidth(self.frame) - CGRectGetWidth(downViewFrame)) / 2.0;
+    downView.frame = downViewFrame;
+}
+
+- (void)setImage:(UIImage *)image forState:(UIControlState)state {
+    [super setImage:image forState:state];
+    [self setNeedsLayout];
+}
+
+- (void)setTitle:(NSString *)title forState:(UIControlState)state {
+    [super setTitle:title forState:state];
+    [self setNeedsLayout];
 }
 
 @end
